6bd2d1b9c2228f58e8296b1c29a97fdb5259a164
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2015-10-01 16:00:55 -0700

    exploring using dplyr

---
+++ nut_explore_dplyr.R
@@ -0,0 +1,18 @@
+library(dplyr)
+library(ggplot2)
+
+nut <- read.csv("~/Desktop/Nutrient_databases/nut_sept22_lwr_dec3.csv", comment.char="#", stringsAsFactors=FALSE)
+
+str(nut)
+summary(nut)
+nut$ASFIS.Scientific.name
+ntbl <- tbl_df(nut)
+ntbl
+glimpse(ntbl)
+(snippet <- subset(nut, ASFIS.Scientific.name == "Abramis brama"))
+
+filter(ntbl, TL < 3)
+filter(ntbl, ASFIS.Scientific.name == "Abramis brama")
+filter(ntbl, ASFIS.Scientific.name %in% c("Abramis brama", "Thymallus arcticus"))
+nut %>% head
+nut %>% head(3)
