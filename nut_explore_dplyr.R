864125efca6737e9b43529d3a67b4198c320632e
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2015-10-03 08:55:05 -0700

    add new Rmd for STAT 545 homework 3

--- nut_explore_dplyr.R
+++ nut_explore_dplyr.R
@@ -18,3 +18,16 @@ nut %>% head
 nut %>% head(3)
 
 select(ntbl, ASFIS.Scientific.name, CA_mg)
+
+
+
+###STAT 545 Homework 3
+
+#1. Get the min and max of x nutrient/body size, for each species
+#2. Look at the spread of x within each habitat (grouping variable)
+#3. Get a trimmed mean, i.e. mean of some sort of subset of the data
+#4. How does nutrient content vary with body size?
+#5. Create some sort of benchmark from the data (i.e. mean, median, quantile, etc.), then determine how many species have a nutrient content less than that
+#6. For each table, make sure to include a relevant figure. 
+# Your figure does not have to depict every last number from the data aggregation result. It just needs to complement the table, add context, and allow for some sanity checking both ways. 
+# Notice which figures are easy/hard to make, which data formats make better inputs for plotting functions vs. for human-friendly tables.
