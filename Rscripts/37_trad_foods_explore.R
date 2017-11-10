d8c632f378c045c6e5865a5cd02efa43d72644b6
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2017-11-09 18:36:05 -0800

    more updates on trad foods analysis and more plots! :tada:

--- Rscripts/37_trad_foods_explore.R
+++ Rscripts/37_trad_foods_explore.R
@@ -6,14 +6,11 @@ library(readxl)
 library(stringr)
 library(purrr)
 library(vegan)
+library(cowplot)
 
 
 nuts_trad <- read_csv("data-processed/trad-foods-cleaned.csv")
 culture_foods <- read_excel("~/Documents/traditional-foods/culture-foods.xlsx")
-
-nuts_mean <- read_csv("data-processed/CINE-inuit-mean-nutrients.csv")
-
-
 
 nuts_trad %>% 
   distinct(latin_name) %>% 
@@ -65,21 +62,11 @@ species_numbers <- nuts_mean %>%
   summarise(n_species = n_distinct(latin_name)) %>% 
   filter(n_species >= 25) 
 
+median(species_numbers$n_species)
+hist(species_numbers$n_species)
 
-# comparing cine inuit to the new data ------------------------------------
-
-
-cine_inuit <- read_csv("data-processed/CINE-inuit-mean-nutrients.csv")
-
-cine_inuit2 <- cine_inuit %>% 
-  mutate(latin_name = str_replace(latin_name, "Mercenaria mercenaria<ca>", "Mercenaria mercenaria"))
-
-inuit_inu <- nuts_mean %>% 
-  filter(culture == "Inuit-Inupiaq") %>% 
-  ungroup() %>% 
-  select(-culture) 
-
-
+nuts_mean %>% 
+  filter(culture == "Haida") %>% View
 
 
 ### ok write out what we've got!
@@ -193,10 +180,93 @@ res <- cnuts_split %>%
 write_csv(res, "data-processed/nut_accumulation_trad_foods.csv")
 res <- read_csv("data-processed/nut_accumulation_trad_foods.csv")
 
-res %>% 
+## now for global dataset
+mean_nuts <- read.csv("data-processed/mean_nuts.csv")
+
+accumulate <- function(data, threshold) {
+  ntbl.RDI.all <- data %>% 
+    mutate(RDI.CA = ifelse(calcium > (1200*threshold), 1, 0)) %>% 
+    mutate(RDI.FE = ifelse(iron > (18*threshold), 1, 0)) %>% 
+    mutate(RDI.ZN = ifelse(zinc > (11*threshold), 1, 0)) %>%
+    mutate(RDI.EPA = ifelse(epa > (1*threshold), 1, 0)) %>%
+    mutate(RDI.DHA = ifelse(dha > (1*threshold), 1, 0)) %>%
+    ungroup() %>% 
+    mutate(RDI.micro.tot = rowSums(.[7:11])) %>% 
+    filter(!is.na(RDI.micro.tot)) 
+  
+  all_spa <- ntbl.RDI.all %>%
+    dplyr::select(species_name, 7:12) %>% 
+    mutate(subgroup = "all") %>% 
+    split( .$subgroup) %>% 
+    map(.f = `[`, c("RDI.CA", "RDI.FE", "RDI.ZN", "RDI.EPA", "RDI.DHA")) %>%
+    map(.f = specaccum, method = "random", permutations = 1000)
+  
+  
+  accumulated_targets <- all_spa %>% 
+    map(.f = `[`, "richness") %>% 
+    unlist() %>% 
+    as.data.frame()
+  
+  accumulated_targets_sd <- all_spa %>% 
+    map(.f = `[`, "sd") %>% 
+    unlist() %>% 
+    as.data.frame()
+  
+  accumulated_targets$richness_level = rownames(accumulated_targets)
+  colnames(accumulated_targets) <- c("number_of_targets", "richness_level")
+  
+  accumulated_targets_sd$sd = rownames(accumulated_targets_sd)
+  colnames(accumulated_targets_sd) <- c("sd", "number_of_targets")
+  
+  accumulated_targets_sd <- accumulated_targets_sd %>% 
+    separate(number_of_targets, into = c("subgroup", "number_of_species")) %>%
+    mutate(number_of_species = str_replace(number_of_species, "sd", "")) %>%
+    mutate(number_of_species = as.numeric(number_of_species))
+  
+  
+  accumulated_targets <- accumulated_targets %>% 
+    separate(richness_level, into = c("subgroup", "number_of_species")) %>%
+    mutate(number_of_species = str_replace(number_of_species, "richness", "")) %>%
+    mutate(number_of_species = as.numeric(number_of_species))
+  
+  accumulated_targets_all <- left_join(accumulated_targets, accumulated_targets_sd)
+  accumulated_targets_all <- accumulated_targets_all %>% 
+    mutate(se = sd / sqrt(number_of_species))
+  return(accumulated_targets_all)
+  
+}
+
+res_global <- accumulate(sample_n(mean_nuts, size = 40, replace = FALSE), threshold = 0.1) %>% 
+  mutate(culture = "global")
+
+
+res_all <- bind_rows(res, res_global)
+
+res_all %>% 
+  filter(culture %in% species_numbers$culture | culture == "global") %>% 
   filter(number_of_species < 11) %>% 
-  ggplot(aes(x = number_of_species, y = number_of_targets, group = culture, color = culture)) + geom_line(size =1.5) +
-  geom_ribbon(aes(ymin = number_of_targets - se, ymax = number_of_targets + se, fill = culture), alpha = 0.2, size = 0) +
+  ggplot(aes(x = number_of_species, y = number_of_targets, group = culture, color = culture)) + geom_line(size =1) +
+  # geom_ribbon(aes(ymin = number_of_targets - se, ymax = number_of_targets + se, fill = culture), alpha = 0.2, size = 0) +
+  ylab("Number of nutrient requirements fulfilled (10% DRI)") +
+  xlab("Number of species") + theme(text = element_text(size=14)) + 
+  # scale_color_grey(start = 0.01, end = 0.7) +
+  # theme_bw() +
+  # theme(legend.position = c(0.6, 0.2)) +
+  # scale_x_continuous(breaks = seq(1,10,1)) +
+  theme(legend.title=element_blank()) + theme_classic() +
+  ylim(0,5) +
+  # facet_wrap( ~ culture) +
+  scale_color_viridis(discrete = TRUE) + scale_fill_viridis(discrete = TRUE)
+
+ggsave("figures/nutrient_accumulation_plots_na_cultures_overlay.png", width = 8, height = 6)
+
+## same figure, no color
+res_all %>% 
+  mutate(culture = ifelse(culture == "global", "Global", culture)) %>% 
+  filter(culture %in% species_numbers$culture | culture == "Global") %>% 
+  filter(number_of_species < 11) %>% 
+  ggplot(aes(x = number_of_species, y = number_of_targets)) + geom_line(size =1) +
+  geom_ribbon(aes(ymin = number_of_targets - se, ymax = number_of_targets + se), alpha = 0.2, size = 0) +
   ylab("Number of nutrient requirements fulfilled (10% DRI)") +
   xlab("Number of species") + theme(text = element_text(size=14)) + 
   # scale_color_grey(start = 0.01, end = 0.7) +
@@ -206,6 +276,58 @@ res %>%
   theme(legend.title=element_blank()) + theme_classic() +
   ylim(0,5) +
   facet_wrap( ~ culture) +
-  scale_color_viridis(discrete = TRUE) + scale_fill_viridis(discrete = TRUE)
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        strip.background = element_blank())
+ggsave("figures/nutrient_accumulation_plots_nut_accum_bw.png", width = 8, height = 6)
 
-ggsave("figures/nutrient_accumulation_plots_na_cultures.png", width = 8, height = 6)
+
+
+
+
+## ok now try to fit a power function to each of these accumulation curves
+
+
+# parameter estimate plots ------------------------------------------------
+
+mod <- res_all %>% 
+  filter(culture %in% species_numbers$culture | culture == "global") %>% 
+  filter(number_of_species < 11) %>% 
+  group_by(culture) %>% 
+  do(tidy(nls(formula = (number_of_targets ~ a * number_of_species^b),data = .,  start = c(a=1, b=0.3)))) 
+
+
+a_terms <- mod %>% 
+  filter(term == "a")
+
+a_plot <- a_terms %>% 
+  ggplot(aes(x = reorder(culture, estimate), y = estimate)) + geom_point() +
+  geom_errorbar(aes(ymin = estimate - std.error, ymax = estimate + std.error)) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  xlab("Culture") + ylab("Parameter estimate (a)") 
+
+
+b_terms <- mod %>% 
+  filter(term == "b")
+
+b_plot <- b_terms %>% 
+  ggplot(aes(x = reorder(culture, estimate), y = estimate)) + geom_point() +
+  geom_errorbar(aes(ymin = estimate - std.error, ymax = estimate + std.error)) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  xlab("Culture") + ylab("Parameter estimate (b)") 
+
+plot_grid(a_plot, b_plot, nrow = 1, ncol = 2)
+
+
+a_est <- mod$estimate[mod$culture == "Haida" & mod$term == "a"]
+b_est <-mod$estimate[mod$culture == "Haida" & mod$term == "b"]
+
+yupik_power_function <- function(x) a_est*x^b_est
+
+
+res_all %>% 
+  filter(number_of_species < 11) %>%
+  filter(culture == "Haida") %>% 
+  ggplot(aes(x = number_of_species, y = number_of_targets)) + geom_line() +
+  ylim(0, 5) +
+  stat_function(fun = yupik_power_function, color = "green")
